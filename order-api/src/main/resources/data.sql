f46444e81b3f40dc8d957944d04c273ea98932e3
takoyummy <takoyummy@naver.com>
2025-06-09 01:01:38 +0900

    chore: 초기 설정 추가

---
+++ order-api/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO product (id, name, price, discount_amount, stock) VALUES (1000000001, '이마트 생수', 800, 100, 1000);
+INSERT INTO product (id, name, price, discount_amount, stock) VALUES (1000000002, '신라면 멀티팩', 4200, 500, 500);
+INSERT INTO product (id, name, price, discount_amount, stock) VALUES (1000000003, '바나나 한 송이', 3500, 300, 200);
+INSERT INTO product (id, name, price, discount_amount, stock) VALUES (1000000004, '삼겹살 500g', 12000, 2000, 100);
+INSERT INTO product (id, name, price, discount_amount, stock) VALUES (1000000005, '오리온 초코파이', 3000, 400, 300);
